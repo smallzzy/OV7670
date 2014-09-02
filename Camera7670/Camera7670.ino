330187fb0fbe53979af3bfbbd3dd5fdc50d37d64
smallzzy <smallzzy@outlook.com>
2014-09-02 18:47:40 +0800

    switch to an 1.8" tft screen
    the serial support will be back when the software for upper machine is
    ready

--- Camera7670/Camera7670.ino
+++ Camera7670/Camera7670.ino
@@ -1,14 +1,12 @@
 #include"config.h"
 
-#define SCCB_SCL 22//PA0
-#define SCCB_SDA 23//PA1
-//#define VSYNC 2
-#define WRST 24//PA2 HREF
-#define WEN 25//PA3
-//#define XCLK 26//PA4
-#define RRST 27//PA5
-#define OE 28//PA6 CS
-#define RCLK 29//PA7 WR RCK
+#define SCCB_SCL 22
+#define SCCB_SDA 23
+#define VSYNC 2
+#define WEN 24
+#define RRST 25
+#define OE 26//CS
+#define RCLK 27//WR RCK
 
 #define D0 37//PC0
 #define D1 36//PC1
@@ -19,30 +17,42 @@
 #define D6 31//PC6
 #define D7 30//PC7
 
+//output mode 0:screen
+static byte outputmode = 0;
+
+#if outputmode == 0
+  #include <TFT.h>  // Arduino LCD library
+  #include <SPI.h>
+  #define cs   53
+  #define dc   48
+  #define rst  49
+  TFT TFTscreen = TFT(cs, dc, rst);
+#endif
+
 volatile int OV_State=0;
-int c_data;
 int incomingByte;
+
 void setup(){
-  Serial.begin(115200);
-  while (!Serial) {
-    ;
+  //output
+  if (outputmode == 0)
+  {
+	  TFTscreen.begin();
+	  TFTscreen.background(0, 0, 0);	  
   }
-  pinMode(WRST,OUTPUT);
+  
+  //connection initialize
   pinMode(WEN,OUTPUT);
   pinMode(RRST,OUTPUT);
   pinMode(OE,OUTPUT);
   pinMode(RCLK,OUTPUT);
   DDRC = 0x00;
+  
+  //disable FIFO, start loading data
   digitalWrite(OE,LOW);
   digitalWrite(WEN,HIGH);
+  
   Sensorinit();
   readReset();
-  while(incomingByte!=255){
-    incomingByte=Serial.read();
-  }
-  digitalWrite(13,HIGH);
-  delay(500);
-  digitalWrite(13,LOW);
 }
 
 void loop(){
@@ -52,21 +62,22 @@ void loop(){
 
   readReset();
   digitalWrite(OE,LOW);
-  Serial.println(0xff);
-  digitalWrite(13,HIGH);
-  delay(500);
-  digitalWrite(13,LOW);
-  delay(10);
-  for(int a=0;a<240;a++){
-    for(int b=0;b<320;b++){
-      digitalWrite(RCLK,LOW); 
-      c_data=PINC;//<<8;//读高位
-      digitalWrite(RCLK,HIGH); 
-      digitalWrite(RCLK,LOW);
-      //c_data|=PINC;//读低位
-      digitalWrite(RCLK,HIGH);
-      Serial.print(c_data);
-    }
+  if (outputmode == 0)
+  {
+	  int colordata;
+	  for(int x=0; x<120; x++)
+	  {
+		  for (int y = 0; y<160;y++)
+		  {
+			  digitalWrite(RCLK,LOW);
+			  colordata=PINC<<8;		//读高位
+			  digitalWrite(RCLK,HIGH);
+			  digitalWrite(RCLK,LOW);
+			  colordata|=PINC;			//读低位
+			  digitalWrite(RCLK,HIGH);
+			  TFTscreen.drawPixel(x,y,colordata);  //RGB565
+		  }
+	  }  
   }
   digitalWrite(OE,HIGH);
   delayMicroseconds(20);
@@ -74,20 +85,16 @@ void loop(){
 }
 
 void watcher(){
-  if(OV_State == 0)           //判断状态，第一次下降沿
+  if(OV_State == 0)				//判断状态，第一次下降沿
   {
-    OV_State = 1;             //状态变为1
-    digitalWrite(WRST,LOW);
-    digitalWrite(WRST,HIGH);
-    digitalWrite(WEN,HIGH);                //使能WEN，写
-    //Serial.println(1000000);
+    OV_State = 1;				//状态变为1
+    digitalWrite(WEN,HIGH);		//使能WEN，写
   }
-  else if(OV_State == 1)      //状态1，
+  else if(OV_State == 1)		//状态1，
   {
-    digitalWrite(WEN,LOW);                 //关闭写使能，禁止写
-    OV_State = 2;             //状态变为2，准备读数据
-    detachInterrupt(0);		//关闭外部中断 
-    //Serial.println(2000000);
+    digitalWrite(WEN,LOW);		//关闭写使能，禁止写
+    OV_State = 2;				//状态变为2，准备读数据
+    detachInterrupt(0);			//关闭外部中断 
   }  
 }
 
@@ -249,7 +256,3 @@ byte SCCB_Read(){
   pinMode(SCCB_SDA,OUTPUT); 
   return data;
 }
-
-
-
-
